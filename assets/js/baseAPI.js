077953ec1bc129afb6c183c2fcfc68656620e398
dongguojuan <1395040604@qq.com>
2020-11-26 10:37:37 +0800

    首页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,25 @@ var baseURL = "http://ajax.frontend.itheima.net"
 // 处理函数
 $.ajaxPrefilter(function (params) {
     params.url = baseURL + params.url;
+
+    //2.针对需要权限的接口配置头像信息
+    // 必须以/my/开头的
+    if(params.url.indexOf('/my/')!==-1) {
+        params.headers={
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+    // 3.拦截所有响应，判断身份认证信息
+    params.complete = function (res) {
+        console.log(res)
+        var obj = res.responseJSON
+        if(obj.status == 1 && obj.message == "身份认证失败！") {
+            // 清空本地token
+            localStorage.removeItem('token')
+            // 页面跳转
+            location.href = '/login.html'
+        }
+        
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function() {
+    // 1.获取用户信息
+    getUserInof()
+    // 退出
+    $('#logout').on('click',function(){
+        // 询问
+        layer.confirm('是否确认退出?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            // 清除token，跳转页面，关闭询问框
+            localStorage.removeItem('token')
+            location.href="/login.html"
+            layer.close(index);
+          });
+        
+    })
+});
+// 获取用户信息(封装入口函数的外面)
+// 原因后面其他的页面也调用
+function getUserInof() {
+    $.ajax({
+        url:'/my/userinfo',
+        // headers: {
+        //     // 重新登录，因为token过期事件12小时
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success: function(res){
+          if(res.status !==0) {
+              return layui.layer.msg(res.massage)
+          }
+          renderAvatar(res.data)
+        }
+    })
+}
+// 封装用户头像渲染函数
+function renderAvatar(user) {
+    // 1.用户名 (昵称优先，没有用username)
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 2.用户头像
+    if(user.user_pic !==null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src',user.user_pic);
+        $('.text-avatar').hide();
+    }else {
+        // 没有头像
+        $('.layui-nav-img').hide();
+        var text = name[0].toUpperCase();
+        $('.text-avatar').show().html(text)
+    }
+}
